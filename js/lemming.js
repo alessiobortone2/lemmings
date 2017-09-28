b0f721a8a751955897d49b1580038239f4373a02
alessiobortone2 <alessiobortone2@gmail.com>
2017-09-28 16:37:14 +0100

    mouse solved

--- js/lemming.js
+++ js/lemming.js
@@ -1,10 +1,11 @@
 (function (exports){
 
 var Lemming = function(spawn){
-  this.shape = Bodies.circle(spawn.x, spawn.y, 10);
+  this.shape = Bodies.circle(spawn.x, spawn.y, 30);
   this.state = "intheair";
   this.shape.restitution = 0;
   this.shape.friction = 0;
+  
   World.add(engine.world, [this.shape]);
 };
 
