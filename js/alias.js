4c0d5182c76c42d4fd29cc9d24f99181a8c063cd
alessiobortone2 <alessiobortone2@gmail.com>
2017-09-28 15:42:35 +0100

    play with mouse

--- js/alias.js
+++ js/alias.js
@@ -1,15 +1,15 @@
 
-(function(exports){
-  exports.Engine = Matter.Engine;
-  exports.World = Matter.World;
-  exports.Composites = Matter.Composites;
-  exports.Composite = Matter.Composite;
-  exports.Body = Matter.Body;
-  exports.Bodies = Matter.Bodies;
-  exports.Events = Matter.Events;
-  exports.MouseConstraint = Matter.MouseConstraint;
-  exports.Mouse = Matter.Mouse;
-  exports.Renderer = Matter.Renderer;
-  exports.Render = Matter.Render;
-  exports.Common = Matter.Common;
-})(this);
+// (function(exports){
+//   exports.Engine = Matter.Engine;
+//   exports.World = Matter.World;
+//   exports.Composites = Matter.Composites;
+//   exports.Composite = Matter.Composite;
+//   exports.Body = Matter.Body;
+//   exports.Bodies = Matter.Bodies;
+//   exports.Events = Matter.Events;
+//   exports.MouseConstraint = Matter.MouseConstraint;
+//   exports.Mouse = Matter.Mouse;
+//   exports.Renderer = Matter.Renderer;
+//   exports.Render = Matter.Render;
+//   exports.Common = Matter.Common;
+// })(this);
--- js/lemming.js
+++ js/lemming.js
@@ -1,16 +1,12 @@
 (function (exports){
-// var Matter = require('../js/lib/matter.js')
-// var Bodies = Matter.Bodies;
 
-  
 var Lemming = function(spawn){
   this.shape = Bodies.circle(spawn.x, spawn.y, 10);
   this.state = "intheair";
   this.shape.restitution = 0;
   this.shape.friction = 0;
-  console.log(this.shape);
   World.add(engine.world, [this.shape]);
 };
-// module.exports = Lemming;
+
 exports.Lemming = Lemming;
 })(this)
