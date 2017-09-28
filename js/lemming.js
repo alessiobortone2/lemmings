3eefee2b5c1220627d8d401d48896dd1aefaec69
Jenny Bacon <jenniferbacon01@gmail.com>
2017-09-28 13:45:21 +0100

    multiple lemmings moving

--- js/lemming.js
+++ js/lemming.js
@@ -2,13 +2,12 @@
 // var Matter = require('../js/lib/matter.js')
 // var Bodies = Matter.Bodies;
 
-  
+
 var Lemming = function(spawn){
   this.shape = Bodies.circle(spawn.x, spawn.y, 10);
   this.state = "intheair";
   this.shape.restitution = 0;
   this.shape.friction = 0;
-  console.log(this.shape);
   World.add(engine.world, [this.shape]);
 };
 // module.exports = Lemming;
