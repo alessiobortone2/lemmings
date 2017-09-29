575d8a912233d728e97b93f794fd0614b5f5672c
Jenny Bacon <jenniferbacon01@gmail.com>
2017-09-29 13:01:51 +0100

    tidying up

--- js/lemming.js
+++ js/lemming.js
@@ -1,18 +1,13 @@
 (function (exports){
-// var Matter = require('../js/lib/matter.js')
-// var Bodies = Matter.Bodies;
-
-
+  
 var Lemming = function(engine, spawn){
-
   var defaultCategory = 0x0001
   var lemmingCategory = 0x0002
   this.shape = Bodies.circle(spawn.x, spawn.y, 10, {collisionFilter: { category: lemmingCategory, mask: defaultCategory}} );
-  // this.state.collisionFilter= -1;
   this.shape.restitution = 0;
   this.shape.friction = 0;
   World.add(engine.world, [this.shape]);
 };
-// module.exports = Lemming;
+
 exports.Lemming = Lemming;
 })(this)
