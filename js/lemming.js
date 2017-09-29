bf74346f65eb31e29d49cf481647b212b6e0d0be
Adam Romano <manoadamro.imp@gmail.com>
2017-09-29 14:28:27 +0100

    removes blocks when lemming is clicked

--- js/lemming.js
+++ js/lemming.js
@@ -1,9 +1,11 @@
 (function (exports){
-  
+
+LEMMING_SIZE = 10
+
 var Lemming = function(engine, spawn){
   var defaultCategory = 0x0001
   var lemmingCategory = 0x0002
-  this.shape = Bodies.circle(spawn.x, spawn.y, 10, {collisionFilter: { category: lemmingCategory, mask: defaultCategory}} );
+  this.shape = Bodies.circle(spawn.x, spawn.y, LEMMING_SIZE, {collisionFilter: { category: lemmingCategory, mask: defaultCategory}} );
   this.shape.restitution = 0;
   this.shape.friction = 0;
   World.add(engine.world, [this.shape]);
